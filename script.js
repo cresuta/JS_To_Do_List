039ba496cc5bd6972ecf889e2b7879b4a9034516
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2021-04-11 20:50:39 -0400

    input field and add btn rearranged, icons appended upon add click

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const addBtn = document.querySelector('.add');
 const inputContainer = document.querySelector('.input');
+const icons = document.querySelector('.icons');
+const todo = document.querySelector('.todo');
+
+const deleteIcon = document.createElement('i');
+const editIcon = document.createElement('i');
+const completedIcon = document.createElement('i');
 
 addBtn.addEventListener('click', () => {
-    let inputField = document.createElement('input');
-    inputContainer.append(inputField);
+    deleteIcon.classList.add('far', 'fa-trash-alt', 'icons');
+    editIcon.classList.add('far', 'fa-edit', 'icons');
+    completedIcon.classList.add('far', 'fa-check-square', 'icons');
+    icons.appendChild(deleteIcon);
+    icons.appendChild(editIcon);
+    icons.appendChild(completedIcon);
+    // let inputField = document.createElement('input');
+    // inputContainer.append(inputField);
 })
